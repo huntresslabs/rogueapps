ac2cacec4be3bbc80cd32d43db272ebef02ac3d6
mattk <huskyhacks.mk@gmail.com>
2024-08-06 09:33:32 -0400

    issue template

---
+++ .github/ISSUE_TEMPLATE/custom.md
@@ -0,0 +1,10 @@
+---
+name: New RogueApp
+about: Describe this issue template's purpose here.
+title: "[New RogueApp]: {RogueApp Name}
+labels: new rogueapp
+assignees: ''
+
+---
+
+* **Contributor Name:**
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=HK+Nova:wght@500;700;800&family=Visuelt:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=HK+Nova:wght@500;700;800&family=Visuelt:wght@400;700&display=swap" />
         </Head>
         <body>
           <Main />
