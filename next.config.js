77d6c6755ac288648c5c2edae8f51e5c8f230fbb
mattk <huskyhacks.mk@gmail.com>
2024-08-06 11:26:50 -0400

    workflow for deploy

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/rogueapps",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
