bf4dc307e1b56ce67dd7f20f0c50f733cd494634
mattk <huskyhacks.mk@gmail.com>
2024-08-06 14:04:01 -0400

    expand meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,23 +7,28 @@ export const metadata = {
   title: 'RogueApps',
   description: 'RogueApps: when good OAuth apps go rogue.',
   viewport: 'width=device-width, initial-scale=1',
+  keywords: 'OAuth, security, rogue apps, Huntress',
+  robots: 'index, follow',
+  themeColor: '#00f49c',
   icons: {
-    icon: 'https://huntresslabs.github.io/rogueapps/favicon.ico',
+    icon: 'https://raw.githubusercontent.com/huntresslabs/rogueapps/main/public/favicon.ico',
   },
   openGraph: {
-    card: 'summary_large_image',
     title: 'RogueApps',
-    author: 'Huntress',
     description: 'RogueApps: when good OAuth apps go rogue.',
+    type: 'website',
+    url: 'https://huntresslabs.github.io/rogueapps',
     image: 'https://cdn.prod.website-files.com/655ddcc107aef728354e9c2a/655ddcc107aef728354e9cbf_Huntress-logo.svg',
+    siteName: 'RogueApps',
+    locale: 'en_US',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'RogueApps',
-    site: '@HuntressLabs',
-    creator: '@HuntressLabs',
     description: 'RogueApps: when good OAuth apps go rogue.',
     image: 'https://cdn.prod.website-files.com/655ddcc107aef728354e9c2a/655ddcc107aef728354e9cbf_Huntress-logo.svg',
+    site: '@HuntressLabs',
+    creator: '@HuntressLabs',
   },
 };
 
@@ -31,17 +36,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <Head>
-        <meta property="og:card" content={metadata.openGraph.card} />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content={metadata.viewport} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="robots" content={metadata.robots} />
+        <meta name="theme-color" content={metadata.themeColor} />
+        <link rel="icon" href={metadata.icons.icon} />
         <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:author" content={metadata.openGraph.author} />
         <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:url" content={metadata.openGraph.url} />
         <meta property="og:image" content={metadata.openGraph.image} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
         <meta name="twitter:card" content={metadata.twitter.card} />
         <meta name="twitter:title" content={metadata.twitter.title} />
-        <meta name="twitter:site" content={metadata.twitter.site} />
-        <meta name="twitter:creator" content={metadata.twitter.creator} />
         <meta name="twitter:description" content={metadata.twitter.description} />
         <meta name="twitter:image" content={metadata.twitter.image} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body>
         <Navbar />
@@ -52,3 +68,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
