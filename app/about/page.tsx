206c31c5313d9a6ade92ab0b8503d39dfa229f99
mattk <huskyhacks.mk@gmail.com>
2024-08-06 10:11:51 -0400

    about and contribute

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,13 @@ export default function About() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>About RogueApps</h1>
-        <p>This website documents instances of OAuth applications that, while not evil outright, are often used maliciously by attackers.</p>
+        <p>The RogueApps project documents when Good Apps Go Rogue.</p>
+        <p>RogueApps are OIDC/OAuth 2.0 applications that, while not explicitly evil, are often abused and used maliciously. This repository documents the emerging attack surface of SaaS, OIDC, and OAuth 2.0 applications that help attackers during intrusions. If the application was not specifically created for evil purposes, but has been observed during identity compromises, it's a RogueApp.</p>
+      
+      
+        <h3>Acknowledgements</h3>
+        <p>RogueApps was inspired by <a href="https://wtfbins.wtf/">WTFBins</a>, which is the excellent work of <a href="https://taggartinstitute.org/">The Taggart Institute / Michael Taggart</a>. Thank you to TTI/Taggart!</p>  
+        <p>RogueApps was created by Matt Kiely, Principal Security Researcher at Huntress, and the Huntress Research Team.</p>
       </main>
     </div>
   );
--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -9,7 +9,8 @@ export default function Contribute() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Contribute to RogueApps</h1>
-        <p>If you have any information about OAuth applications that are often used maliciously, please reach out to us.</p>
+        <p>We'd love to hear your stories of RogueApps that you've seen!</p>
+        <p>If you want to contribue to the RogueApps project, please <a href="https://github.com/huntresslabs/rogueapps/issues/new?assignees=&labels=new+rogueapp&projects=&template=custom.md&title=%5BNew+RogueApp%5D%3A+%28RogueApp+Name%29">open an Issue in the official GitHub repository.</a> Please follow the issue template and include details about the observed TTPs for the RogueApp. <b>Please do not submit any sensitive, private, or proprietary information.</b></p>
       </main>
     </div>
   );
